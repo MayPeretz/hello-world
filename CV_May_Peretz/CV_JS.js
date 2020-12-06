50c55017b6f4a4ef6169443bd261d8f00667e527
May <may.peretz123@gmail.com>
2020-12-06 19:38:13 +0200

    cv js

---
+++ CV_May_Peretz/CV_JS.js
@@ -0,0 +1,12 @@
+function change2 (){
+    document.getElementById("southA").src="southamrica2.jpg";
+    }
+    function change1 (){
+    document.getElementById("southA").src="try1.jpg";
+    }
+    function change3 (){
+        document.getElementById("MarocPhilipines").src="maroco2.jpg";
+        }
+        function change4 (){
+        document.getElementById("MarocPhilipines").src="maroco1.jpg";
+        }
